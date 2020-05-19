688a168fc68e0bb04102e7e9ac2ce84ecdad2c96
mohamed nagy <mn6373357@gmail.com>
2020-05-19 18:01:23 +0200

    second part

---
+++ Pharma_Second_Part/Pharma/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Pharma.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Pharma.DAL.PharmaContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Pharma.DAL.PharmaContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
