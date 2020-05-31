7bc13962131c8e34ffc6f8821309610414ea6a80
mariaamm <mariaam.wael05@gmail.com>
2020-05-31 18:01:12 +0200

    tweleve part

---
+++ Pharma_Twelve_part/Pharma/Models/Cart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+
+namespace Pharma.Models
+{
+    public class Cart
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
+        [ForeignKey("Product")]
+        public int Product_id { get; set; }
+        public DateTime Added_To { get; set; }
+        public virtual Product Product { get; set; }
+
+    }
+}
